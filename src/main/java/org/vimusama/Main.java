46390eb5a6852373a1b8d74b8bae9313cdfcf325
Vimarsh Sharma <vimarsh.sharma@antcreatives.com>
2025-04-28 17:57:41 +0530

    Made many changes to database in one go with a single transaction(experimental excercise)

--- src/main/java/org/vimusama/Main.java
+++ src/main/java/org/vimusama/Main.java
@@ -10,7 +10,7 @@ import org.hibernate.cfg.Configuration;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        User newUser = new User(101,"vimusama",
+        User newUser = new User(103,"vimusamaTrial",
                 "aPassword",
                 "normalUser",
                 true);
@@ -20,9 +20,24 @@ public class Main {
         try{
             SessionFactory factory = configuration.buildSessionFactory();
             Session session = factory.openSession();
-            Transaction transaction = session.beginTransaction() ;
+            Transaction createTransaction = session.beginTransaction() ;
             session.persist(newUser);
-            transaction.commit() ;
+//            createTransaction.commit() ;
+//            Transaction getTransaction = session.beginTransaction() ;
+            User temp = session.find(User.class, 6) ;
+//            getTransaction.commit();
+//            Transaction deleteTransaction = session.beginTransaction() ;
+            session.remove(temp) ;
+//            deleteTransaction.commit();
+            User updatedUser = new User(102,"vimusamaUpdated",
+                    "aPassword",
+                    "normalUser",
+                    true);
+//            Transaction updateTransaction = session.beginTransaction();
+            session.merge(updatedUser) ;
+//            updateTransaction.commit() ;
+
+            createTransaction.commit() ;
         }
         catch(Exception e){
             System.out.println("An error occured-> " + e) ;
